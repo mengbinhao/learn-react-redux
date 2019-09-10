05c42cc4530df991231ae219656e01f00efb1e69
jack <mengbinhao2018@gmail.com>
2019-09-10 14:17:26 +0800

    finish add、delete、change logic

--- src/TodoList.js
+++ src/TodoList.js
@@ -6,11 +6,20 @@ class TodoList extends Component {
     return (
       <Fragment>
         <div>
-          <input value={this.props.inputValue} />
-          <button>button</button>
+          <input
+            value={this.props.inputValue}
+            onChange={this.props.changeVal}
+          />
+          <button onClick={this.props.addItem}>button</button>
         </div>
         <ul>
-          <li>jack</li>
+          {this.props.items.map((item, index) => {
+            return (
+              <li key={index} onClick={() => this.props.deleteItem(index)}>
+                {item}
+              </li>
+            )
+          })}
         </ul>
       </Fragment>
     )
@@ -19,11 +28,37 @@ class TodoList extends Component {
 
 const mapStateToProps = state => {
   return {
-    inputValue: state.inputValue
+    inputValue: state.inputValue,
+    items: state.items
+  }
+}
+
+const mapDispatchToProps = disptch => {
+  return {
+    changeVal(e) {
+      const action = {
+        type: 'changeVal',
+        value: e.target.value
+      }
+      disptch(action)
+    },
+    addItem() {
+      const action = {
+        type: 'addItem'
+      }
+      disptch(action)
+    },
+    deleteItem(index) {
+      const action = {
+        type: 'deleteItem',
+        value: index
+      }
+      disptch(action)
+    }
   }
 }
 
 export default connect(
   mapStateToProps,
-  null
+  mapDispatchToProps
 )(TodoList)
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -4,5 +4,24 @@ const defaultState = {
 }
 
 export default (state = defaultState, action) => {
+  if (action.type === 'changeVal') {
+    const newState = JSON.parse(JSON.stringify(state))
+    newState.inputValue = action.value
+    return newState
+  }
+
+  if (action.type === 'addItem') {
+    const newState = JSON.parse(JSON.stringify(state))
+    newState.items.push(newState.inputValue)
+    newState.inputValue = ''
+    return newState
+  }
+
+  if (action.type === 'deleteItem') {
+    const newState = JSON.parse(JSON.stringify(state))
+    newState.items.splice(action.value, 1)
+    return newState
+  }
+
   return state
 }
